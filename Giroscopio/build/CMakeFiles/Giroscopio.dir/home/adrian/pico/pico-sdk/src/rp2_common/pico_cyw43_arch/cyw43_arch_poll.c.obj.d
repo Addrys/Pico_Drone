7e6df91c824a48bd29d673b32cc6795172dfa23f
Addrys <adrianamarillagomez@gmail.com>
2023-08-23 02:07:41 +0200

    Giroscopio

---
+++ Giroscopio/build/CMakeFiles/Giroscopio.dir/home/adrian/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/Giroscopio.dir/home/adrian/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.obj: \
+ /home/adrian/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
